56ec447a7a90a575f5decc6c42aac172127230e8
Edward Joseph <dev.edwardjoseph@gmail.com>
2022-02-12 21:22:52 +0330

    Create template

---
+++ config/webpack.dev.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+	mode: 'development',
+	stats: 'errors-only',
+
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.[contenthash:8].js'
+	},
+
+	devServer: {
+		port: 3000,
+		hot: true,
+		client: {
+			logging: 'error',
+		},
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: __dirname + '/../public/index.html',
+		}),
+	],
+});
